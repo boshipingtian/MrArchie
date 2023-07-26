385520118fb8bab5ba9d87e7ddbc6235dd8d8f9f
1619794359@qq.com <1619794359@qq.com>
2023-07-26 14:49:55 +0800

    feat 添加native-ui

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import App from './App.vue'
 // 导入上面新建的路由文件
 import router from './router/index'
 
+// 通用字体
+import 'vfonts/Lato.css'
+// 等宽字体
+import 'vfonts/FiraCode.css'
+
 const app = createApp(App)
 app.use(router)
+app.use(naive)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from "path";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +11,11 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
     }
-  },
-  plugins: [vue()],
+  }, plugins: [vue(), AutoImport({
+    imports: ['vue', {
+      'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
+    }]
+  }), Components({
+    resolvers: [NaiveUiResolver()]
+  })],
 })
