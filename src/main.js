b556780bd17880e8936889b2b1755c5081b0ef7b
1619794359@qq.com <1619794359@qq.com>
2023-07-29 16:54:30 +0800

    feat 添加axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 // 导入上面新建的路由文件
 import router from './router/index'
+import naive from "naive-ui";
+import axios from './config/axios';
 
 // 通用字体
 import 'vfonts/Lato.css'
@@ -12,4 +14,5 @@ import 'vfonts/FiraCode.css'
 const app = createApp(App)
 app.use(router)
 app.use(naive)
+app.config.globalProperties.$axios = axios;
 app.mount('#app')
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,25 +1,19 @@
 const routes = [
-  {
-    path: '/',
-    title: '首页',
-    component: () => import('@/pages/index.vue'),
-  },
-  {
-    path: '/tools/',
-    children: [
-      {
-        // 当 /user/:id/profile 匹配成功
-        // UserProfile 将被渲染到 User 的 <router-view> 内部
-        path: 'profile',
-        component: () => import('@/pages/tools/DrawApiTool.vue'),
-      },
-      {
-        // 当 /user/:id/posts 匹配成功
-        // UserPosts 将被渲染到 User 的 <router-view> 内部
-        path: 'posts',
-        component: () => import('@/pages/tools/DrawApiTool.vue'),
-      },
-    ],
-  },
+    {
+        path: '/',
+        title: '首页',
+        component: () => import('@/pages/index.vue'),
+    },
+    {
+        path: '/tools/',
+        children: [
+            {
+                // 当 /user/:id/profile 匹配成功
+                // UserProfile 将被渲染到 User 的 <router-view> 内部
+                path: 'cron',
+                component: () => import('@/pages/tools/cron/index.vue'),
+            }
+        ],
+    },
 ]
 export default routes
