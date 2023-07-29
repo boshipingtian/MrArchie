12defccd7f6dc6c06436567bbdd1f20d39049db5
MrArchie <1619794359@qq.com>
2023-07-29 23:17:24 +0800

    feat 全局修改亮色主题

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <script setup>
+import {provide, ref} from 'vue'
+import {useOsTheme, darkTheme} from 'naive-ui'
+
+const theme = ref(null);
+const osTheme = useOsTheme()
+theme.value = osTheme?.value === 'dark' ? darkTheme : null;
+const themeFunc = () => {
+  return theme.value = theme.value === null ? darkTheme : null;
+}
+provide('theme', theme)
 </script>
 
 <template>
-  <router-view></router-view>
+  <n-config-provider :theme="theme">
+    <n-global-style />
+    <n-card> 当前操作系统的主题是 {{ osTheme }}。
+      <n-button @click="themeFunc">切换</n-button>
+    </n-card>
+    <router-view></router-view>
+  </n-config-provider>
 </template>
 
 <style scoped>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,15 @@
 <script setup>
+import {useOsTheme, darkTheme} from 'naive-ui'
+const theme = inject('theme');
+const changeTheme = () => {
+  theme.value = theme.value === null ? darkTheme : null;
+}
 
 </script>
 
 <template>
   <div>首页</div>
+  <n-button @click="changeTheme">按钮</n-button>
 </template>
 
 <style scoped>
